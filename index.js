c980afd9121b293c001fcb5ed6063d19d65e49aa
Jonathan Phanthawong <164473372+jonathanPhan17@users.noreply.github.com>
2024-07-30 17:37:04 -0700

    update feature

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import sqlite3 from 'sqlite3';
+import express from "express";
+import cors from "cors";
+import sqlite3 from "sqlite3";
 
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 // cors elimination middleware
 app.use(cors());
@@ -16,59 +16,83 @@ let db = null;
 function dbSetup(doInsert) {
   if (db === null) {
     return;
-  } 
+  }
 
-  db.run(` 
-      CREATE TABLE IF NOT EXISTS ShoppingList (
-          id integer PRIMARY KEY AUTOINCREMENT,
-          item TEXT NOT NULL,
-          quantity INTEGER NOT NULL
-      );  
-  `)
+  db.run(`
+        CREATE TABLE IF NOT EXISTS ShoppingList (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            item TEXT NOT NULL,
+            quantity INTEGER NOT NULL
+        );
+    `);
 
   if (doInsert) {
     db.run(`
-        INSERT INTO ShoppingList (item, quantity)
-        VALUES ('Eggs', 12),
-               ('Milk', 2); 
-    `);
+            INSERT INTO ShoppingList (item, quantity)
+            VALUES ('Eggs', 12),
+                   ('Milk', 2);
+        `);
   }
-
 }
 
-function appStartedCallback(doInsert) {
-  console.log(`App is listening, PORT ${port}`);
-  db = new sqlite3.Database('shoppingList.db');
+function appStartedCallback() {
+  console.log("App is listening");
+  db = new sqlite3.Database("shopping_list.db");
   dbSetup(false);
 }
 
 app.listen(port, appStartedCallback);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({ status: true });
-})
+});
 
-app.get('/api/list', (req, res) => {
-  db.all('SELECT * FROM ShoppingList', (error, rows) => {
-      if (error) {
-        res.status(500).json({ error });
-      } else {
-        res.status(200).json({ rows });
-      }
+function dbResponseHandler(error, res) {
+  if (error) {
+    res.status(500).json({ error });
+  } else {
+    res.status(200).json({ status: true });
+  }
+}
+
+app.get("/api/list", (req, res) => {
+  db.all("SELECT * FROM ShoppingList", (error, rows) => {
+    if (error) {
+      res.status(500).json({ error });
+    } else {
+      res.status(200).json(rows);
+    }
   });
-})
+});
 
-app.post('/api/list/new', (req, res) => {
-  db.run(`
+app.post("/api/list/new", (req, res) => {
+  db.run(
+    `
         INSERT INTO ShoppingList (item, quantity)
-        VALUES (?, ?);
-  `,
-   [req.body.item, req.body.quantity]);
-   ((error, response) => {
-      if (error) {
-        res.status(500).json({ error });
-      } else {
-        res.status(201).json({ response });
-      }
-   })
-})
+        VALUES (?, ?);        
+    `,
+    [req.body.item, req.body.quantity],
+    (error, _) => dbResponseHandler(error, res)
+  );
+});
+
+app.delete("/api/list/:id", (req, res) => {
+  db.run(
+    `DELETE FROM ShoppingList
+         WHERE id = ?`,
+    [req.params.id],
+    (error, _) => dbResponseHandler(error, res)
+  );
+});
+
+app.put("/api/list/:id", (req, res) => {
+  db.run(
+    `
+            UPDATE ShoppingList
+            SET item = ?, quantity = ?
+            WHERE id = ?
+        `,
+    [req.body.item, req.body.quantity, req.params.id],
+    (error, _) => dbResponseHandler(error, res)
+  );
+});
